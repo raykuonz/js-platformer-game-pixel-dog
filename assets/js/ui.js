96038dbc57d1d88f76abd1c820c177b2bcb627da
Ray Kuo <ray.kuo@salesforce.com>
2023-10-17 00:04:01 +1300

    Replace font VT323

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -2,7 +2,7 @@ export class UI {
      constructor(game) {
         this.game = game;
         this.fontSize = 24;
-        this.fontFamily = 'Pixelify Sans';
+        this.fontFamily = 'VT323';
         this.livesImage = document.getElementById('lives');
         this.backgroundColor = 'rgba(0,0,0,0.5)';
      }
@@ -33,7 +33,7 @@ export class UI {
          // Game over messages
          if (this.game.gameOver) {
                context.textAlign = 'center';
-               context.font = this.fontSize * 2 + 'px ' + this.fontFamily;
+               context.font = this.fontSize * 3 + 'px ' + this.fontFamily;
 
                context.fillStyle = this.backgroundColor;
                context.fillRect(0, 0, this.game.width, this.game.height);
@@ -42,25 +42,25 @@ export class UI {
                context.shadowColor = 'black';
 
                context.fillText('GAME OVER', this.game.width * 0.5, this.game.height * 0.5 - 20);
-               context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+               context.font = this.fontSize * 1.2 + 'px ' + this.fontFamily;
                context.fillText('Refresh browser to retry', this.game.width * 0.5, this.game.height * 0.5 + 20);
          }
 
-         context.font = this.fontSize + 'px ' + this.fontFamily;
+         context.font = this.fontSize * 1.2 + 'px ' + this.fontFamily;
          context.textAlign = 'left';
 
          // Score
          context.fillText(`Score: ` + this.game.score, 20, 50);
 
          // Timer
-         context.font = this.fontSize * 0.8 + 'px ' + this.fontFamily;
+         context.font = this.fontSize + 'px ' + this.fontFamily;
          context.fillText(`Time: ` + (this.game.time * 0.001).toFixed(1), 20, 80);
 
 
          // Game guides
          if (!this.game.init) {
             context.textAlign = 'center';
-            context.font = this.fontSize * 1.2 + 'px ' + this.fontFamily;
+            context.font = this.fontSize * 1.5 + 'px ' + this.fontFamily;
 
             context.fillText('ARROW KEYS TO MOVE', this.game.width * 0.5, this.game.height * 0.5 - 15);
 
